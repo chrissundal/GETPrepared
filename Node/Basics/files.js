c7f5ed5286dc77d089229c554d03450f54184780
chrissundal <christoffersj@hotmail.com>
2025-02-02 15:04:24 +0100

    starting with node

---
+++ Node/Basics/files.js
@@ -0,0 +1,47 @@
+﻿const fs = require('fs')
+const os = require("node:os");
+
+//read files
+// fs.readFile('./docs/blog1.txt', (err, data) => {
+//     if (err) {
+//         console.error(err)
+//     }
+//     console.log(data.toString())
+// })
+//
+// console.log('last line')
+
+//write files
+
+// fs.writeFile('./docs/blog1.txt', 'Hello World!', () => {
+//     console.log('file was changed successfully.');
+// })
+// fs.writeFile('./docs/blog2.txt', 'Hello Again!', () => {
+//     console.log('file was created successfully.');
+// })
+
+//directory
+if (!fs.existsSync('./assets')) {
+    fs.mkdir('./assets', (err) => {
+        if (err) {
+            console.log(err);
+        }
+        console.log('folder created')
+    })
+}
+else {
+    console.log('already exists!');
+    fs.rmdir('./assets', (err) => {
+        console.log('folder removed');
+    })
+}
+//delete files
+
+if (fs.existsSync('./docs/deleteme.txt')) {
+    fs.unlink('./docs/deleteme.txt', (err) => {
+        if (err) {
+            console.log(err);
+        }
+        console.log('file removed')
+    });
+}
---
+++ Node/Basics/streams.js
@@ -0,0 +1,13 @@
+﻿const fs = require('fs');
+
+const readStream = fs.createReadStream('./docs/blog3.txt', 'utf8');
+const writeStream = fs.createWriteStream('./docs/blog4.txt');
+// readStream.on('data', (chunk) => {
+//     console.log('----------- New chunk----------------');
+//     console.log(chunk);
+//     writeStream.write('\nNew chunk\n');
+//     writeStream.write(chunk);
+// })
+
+//piping
+readStream.pipe(writeStream);
