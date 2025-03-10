7e75ff0f06f150655bbe44d84034d3c1ff95ff1b
chrissundal <christoffersj@hotmail.com>
2025-03-10 14:09:38 +0100

    u

--- React/first-look/src/Navbar.js
+++ React/first-look/src/Navbar.js
@@ -1,10 +1,14 @@
+import {Link} from "react-router-dom";
+
 const Navbar = () => {
 	return (
 		<nav className="navbar">
 			<h1>Test navbar</h1>
 			<div className="links">
-				<a href="/">Home</a>
-				<a href="/create">New</a>
+				{/*<a href="/">Home</a>*/}
+				{/*<a href="/create">New</a>*/}
+				<Link to={"/"}>Home</Link>
+				<Link to={"/create"}>New</Link>
 			</div>
 		</nav>
 	);
