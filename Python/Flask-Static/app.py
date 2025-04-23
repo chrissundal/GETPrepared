2c40573c65490d64bc05e16f7cee711c14f7ba1f
chrissundal <christoffersj@hotmail.com>
2025-04-23 11:25:53 +0200

    x

---
+++ Python/Flask-Static/app.py
@@ -0,0 +1,11 @@
+﻿from flask import Flask
+from admin.second import second
+
+app = Flask(__name__)
+app.register_blueprint(second, url_prefix='/admin')
+@app.route('/')
+def home():
+    return "<h3>Hello World!</h3>"
+
+if __name__ == '__main__':
+    app.run()
